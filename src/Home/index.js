4310a7a930d802a01ed5ae229d6ab40a39d30f9c
Arilson Souza <arilsons61@gmail.com>
2021-10-20 15:25:18 -0300

    Add react-dropzone-uploader as submodule

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
+import { DropzoneArea } from 'react-dropzone-uploader';
 import './home.scss'
 import { CardProduct } from '../_components'
 
@@ -11,18 +11,28 @@ const Home = ({ products: { items, totalItems }, dispatch }) => {
       <div className='flex'>
         <div className='flex-1'>
           <h4 className='products__count'>
-							{ totalItems } items
-          </h4>          
+            {totalItems} items
+          </h4>
         </div>
       </div>
 
       <div className='products flex flex-wrap'>
-        { items.map((product, index) => (
+        {items.map((product, index) => (
           <div className='product__item' key={`${product.code_color}-${index}`}>
-            <CardProduct product={product}/>
+            <CardProduct product={product} />
           </div>
         ))}
       </div>
+
+      <DropzoneArea
+        filesLimit={1}
+        acceptedFiles={[
+          '.xlsx',
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        ]}
+        maxFileSize={10000000}
+        showAlerts={[]}
+      />
     </div>
   )
 }
